a9ffcda083a5dad77f63f674df53e0d4e0225c98
Jacopo Esposito <s287141@studenti.polito.it>
2025-11-08 19:14:25 +0100

    feature: integrate Leaflet map functionality with GeoJSON support and report form in MapPage component

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,21 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { BrowserRouter } from 'react-router';
 
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import icon from 'leaflet/dist/images/marker-icon.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
+
+let DefaultIcon = L.icon({
+    iconUrl: icon,
+    shadowUrl: iconShadow,
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    tooltipAnchor: [16, -28],
+    shadowSize: [41, 41]
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>

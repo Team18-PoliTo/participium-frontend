2ab036bbba545832a27eb6b88861eb7ae7cc2baa
MatteoR02 <matteorosato02@gmail.com>
2025-12-07 14:54:58 +0100

    Technical debt: Fixed issue about RoleAssignmentModal

--- src/components/SetUpUserModal.jsx
+++ src/components/SetUpUserModal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import PropTypes from "prop-types";
 import { Form, Button, Row, Col, Alert, Modal } from "react-bootstrap";
 import "./styles/SetUpUserModal.css";
